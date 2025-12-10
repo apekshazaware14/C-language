ff9902688fecb3d8d186750f046bc6d22fb1e7cd
apekshazaware14 <apekshazaware13@gmail.com>
2025-12-10 12:21:55 +0530

    Add files via upload

---
+++ interchange.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter two numbers");
+	scanf("%d %d",&a,&b);
+	c=a;
+	a=b;
+	b=c;
+	printf("value of a=%d",a);
+	printf("\nvalue of b=%d",b);
+}
