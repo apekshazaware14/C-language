bd3144e4e4e74edecc933d634149995ea82ecc77
apekshazaware14 <apekshazaware13@gmail.com>
2025-12-14 20:24:09 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=50,b=20;
+	printf("addition=%d",a+b);
+	printf("\nsubstraction=%d",a-b);
+	printf("\nmultiplication=%d",a*b);
+	printf("\ndivision=%d",a/b);
+	printf("\nmode=%d",a%b);
+}
