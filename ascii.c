7c56adf0c6ef6275ad980e6efd2c6ab641f3cb2b
apekshazaware14 <apekshazaware13@gmail.com>
2025-12-18 19:48:01 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	char a;
+	printf("enter character");
+	scanf("%d",&a);
+    if(a>=65&&a<=90)
+	printf("uppercase letter");
+	else if(a>=97&&a<=122)
+	printf("lowercase letter");
+	
+	
+	
+}
