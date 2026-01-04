334cb8c6222a32687c331388ed0c1b1cf4f10ab0
apekshazaware14 <apekshazaware13@gmail.com>
2026-01-04 20:04:23 +0530

    Add files via upload

---
+++ divisible.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter number");
+	scanf("%d",&a);
+	if(a%3==0&&a%5==0)
+	printf("number is divisible");
+	else
+	printf("number is not divisible");
+	
+}
