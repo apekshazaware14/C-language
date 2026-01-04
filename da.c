e3a2bce623b307e55eea21933e911da64e8ca969
apekshazaware14 <apekshazaware13@gmail.com>
2026-01-04 20:05:27 +0530

    Add files via upload

---
+++ da.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int sal,DA,RA,B;
+	printf("enter salary");
+	scanf("%d",&sal);
+	DA=sal*10/100;
+	RA=sal*20/100;
+	B=sal*3/100;
+	sal=sal+DA+RA+B;
+	printf("salary=%d",sal);
+}
