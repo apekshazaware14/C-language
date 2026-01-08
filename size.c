89366e6f6469c4064c4e1c42811997eef44a8a00
apekshazaware14 <apekshazaware13@gmail.com>
2026-01-08 20:06:14 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("size of int is %d",sizeof(int));
+	printf("\nsize of char is %d",sizeof(char));
+	printf("\nnsize of float is %u",sizeof(float));
+	printf("\nsize of long is %i",sizeof(long));
+	printf("\nsize of double is %d",sizeof(double));
+}
